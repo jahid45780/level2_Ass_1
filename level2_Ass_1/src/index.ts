3cdfce293f66e30ffa7a3c4616805c55121a9984
jahid45780 <jahidhossain88899@gmail.com>
2025-05-12 00:27:11 +0600

    plm 1

--- level2_Ass_1/src/index.ts
+++ level2_Ass_1/src/index.ts
@@ -1,6 +1,12 @@
 {
 
-   
+   function formatString(input: string, toUpper: boolean = true): string {
+  if (toUpper) {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
         
 
     }
