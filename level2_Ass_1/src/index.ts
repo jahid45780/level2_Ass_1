f5640e6d6723d2449d3eb48b00485528dad82de9
jahid45780 <jahidhossain88899@gmail.com>
2025-05-13 01:03:13 +0600

    add the plm 2 and 3

--- level2_Ass_1/src/index.ts
+++ level2_Ass_1/src/index.ts
@@ -1,5 +1,7 @@
 {
 
+  // plm 1
+
    function formatString(input: string, toUpper: boolean = true): string {
   if (toUpper) {
     return input.toUpperCase();
@@ -7,6 +9,50 @@
     return input.toLowerCase();
   }
 }
+
+console.log(formatString("Hello"));         
+console.log(formatString("Hello", true));   
+console.log(formatString("Hello", false));
+
+
+ // plm 2
+
+
+ function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+  return items.filter(item => item.rating >= 4);
+}
+
+
+const gadegtsList = [
+  { title: "laptop", rating: 2.1 },
+  { title: " mobile ", rating: 3.2 },
+  { title: " I phone ", rating: 9.8 }
+];
+
+console.log(filterByRating(gadegtsList));
+
+
+ // plm 3
+
+ function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+
+ console.log(concatenateArrays(["abul", "cabul"], ["tabul"]));
+
+ // plm 4
+
+
+  
+
+
+
+
+
+
+
+
+
         
 
     }
