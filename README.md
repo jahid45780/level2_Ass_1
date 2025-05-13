73113d257be882a99d854c126f98a6cc2ed65bcb
jahid45780 <jahidhossain88899@gmail.com>
2025-05-14 00:49:47 +0600

    add the plm sloved all

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-hii devloper kemon aso
+What are some differences between interfaces and types in TypeScript?
+Ans: Interface
+  *  Used to define object shapes
+  * Supported – multiple interfaces with the same name are merged
+
+   Type
+   *	Can define objects, unions, primitives, and more
+   *    Not supported
+
+   What is the use of the keyof keyword in TypeScript? Provide an example.
+   ans: In TypeScript, the keyof keyword is used to get a union type of all property names keys of a given object type.
+
+   Explain the difference between any, unknown, and never types in TypeScript.
+   ans: any
+    	No type checking; do anything
+        unknown
+        	Like any but safer with required type checks
+            never
+            	Represents values that never occur
+
+
+ What is the use of enums in TypeScript? Provide an example of a numeric and string enum.
+
+  TypeScript is used to define a set of named constants.
+It makes code more readable, organized, and type-safe when working with a fixed set of related values like days, statuses, directions,
+
+exple
+
+enum Status {
+  Success = "SUCCESS",
+  Error = "ERROR",
+  Pending = "PENDING"
+}
+
+let currentStatus: Status = Status.Success;
+console.log(currentStatus);
+
+
+What is type inference in TypeScript? Why is it helpful?
+ans:
+Type Inference is a feature in TypeScript where the compiler automatically infers detects the type of a variable based on its value, without needing you to explicitly declare the type
+
+
+Provide an example of using union and intersection types in TypeScript.
+Union Type
+function printId(id: string | number) {
+  console.log("ID:", id);
+}
+
+Intersection Type
+
+type User = {
+  name: string;
+};
+
+type Admin = {
+  role: string;
+};
+
+type AdminUser = User & Admin;
+
+const user1: AdminUser = {
+  name: "Jahid",
+  role: "Super Admin"
+};
+
--- level2_Ass_1/src/index.ts
+++ level2_Ass_1/src/index.ts
@@ -42,10 +42,136 @@ console.log(filterByRating(gadegtsList));
 
  // plm 4
 
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  public getInfo(): string {
+    return `The make is ${this.make} and year is ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  public getModel(): string {
+    return `The model is ${this.model}`;
+  }
+}
+
+const yourCar = new Car("Honda", 2024, "DO4N");
+
+console.log(yourCar.getInfo());   
+console.log(yourCar.getModel());
+
+// plm 5
+
+function processValue (value: string | number) : number{
+     if (typeof value === "string"){
+         return value.length
+     } else{
+       return value * 2
+     }
+}
+
+console.log(processValue("howAreYou"));
+console.log(processValue(2));
+
+// plm 6
+
+interface product{
+  name: string;
+  price: number
+}
+
+function getMostExpensiveProduct (products: product[]): product | null{
+  if(products.length === 0){
+    return null
+  }
+
+  let mostExpensive = products[0]
+
+
+  for (let i= 1; i < products.length; i ++){
+      if(products[i].price > mostExpensive.price ){
+          mostExpensive = products[i]
+      }
+  }
+
+  return mostExpensive
+
+}
+
+const products: product[] = [
+  { name: " Nosto laptop", price: 100 },
+  { name: "book", price: 205 },
+  { name: "phone", price: 20 }
+];
+
+console.log(getMostExpensiveProduct(products));
+
+// plm 7
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+function getDayType(day : Day): string{
+   if( day === Day.Saturday || day === Day.Sunday ){
+      return "Weekend"
+   } else{
+     return "Weekend"
+   }
+}
+
+console.log(getDayType(Day.Saturday));  
+console.log(getDayType(Day.Sunday));  
 
   
+  // plm 8
+
+  async function squareAsync(n:number): Promise<number>{
+      if(n < 0){
+          throw new Error(" Dear User Negative numbers are not allowed")
+      }
+
+      return new Promise((resolve)=>{
+          setTimeout(()=>{
+              resolve(n*n);
+          }, 1000)
+      })
+  }
+
+  squareAsync(4)
+  .then(result => console.log(result) )
+  .catch(Error => console.error(Error))
+ 
 
 
+ 
+   
+
+   
+
+
+
+}
 
 
 
@@ -55,5 +181,5 @@ console.log(filterByRating(gadegtsList));
 
         
 
-    }
+    
 
